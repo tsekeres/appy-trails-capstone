c1e6286ca34761ecc3a5041d11d9478d8bcc7d95
Tad Sekeres <74842096+tsekeres@users.noreply.github.com>
2021-06-01 12:46:23 -0500

    Initial commit

---
+++ src/views/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// import UserInfoCard from '../components/UserInfo';
+
+function Home({ user }) {
+  return (
+    <div className='user-welcome'>
+      <h1>Welcome to </h1>
+      <hr></hr>
+      {user ? <h2>Come On In</h2> : <h2>Please Log In</h2>}
+    </div>
+  );
+}
+
+Home.propTypes = {
+  user: PropTypes.any,
+};
+
+export default Home;
