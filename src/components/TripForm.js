7d5406019e353fb81d33ac10deddf24cd0cf73a7
tsekeres <twsekeres@gmail.com>
2021-06-06 15:25:20 -0500

    all views with content and read

---
+++ src/components/TripForm.js
@@ -0,0 +1,159 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import PropTypes from 'prop-types';
+
+const TripForm = ({
+  formTitle,
+  setProjects,
+  title,
+  screenshot,
+  netlifyLink,
+  githubLink,
+  loomLink,
+  description,
+  techUsed,
+  firebaseKey,
+}) => {
+  const [project, setProject] = useState({
+    title: title || '',
+    screenshot: screenshot || '',
+    netlifyLink: netlifyLink || '',
+    githubLink: githubLink || '',
+    loomLink: loomLink || '',
+    description: description || '',
+    techUsed: techUsed || '',
+    firebaseKey: firebaseKey || '',
+  });
+  const history = useHistory();
+
+  const handleInputChange = (e) => {
+    setProject((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (project.firebaseKey) {
+      updateProject(project).then(setProjects);
+    } else {
+      addProject(project).then(setProjects);
+      history.push('/projects');
+
+      setProject({
+        title: '',
+        screenshot: '',
+        netlifyLink: '',
+        githubLink: '',
+        loomLink: '',
+        description: '',
+        techUsed: '',
+        firebaseKey: null,
+      });
+    }
+  };
+
+  return (
+    <div className='project-form'>
+      <Form id='addProjectForm' autoComplete='off' onSubmit={handleSubmit}>
+        <h2>{formTitle}</h2>
+        <FormGroup>
+          <Label for='title'>Title:</Label>
+          <Input
+            name='title'
+            id='title'
+            value={project.title}
+            type='text'
+            placeholder='Enter a Title'
+            onChange={handleInputChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for='screenshot'>Screenshot: </Label>
+          <Input
+            name='screenshot'
+            id='screenshot'
+            value={project.screenshot}
+            type='img'
+            placeholder='Enter a Screenshot'
+            onChange={handleInputChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for='netlifyLink'>Netlify Link: </Label>
+          <Input
+            name='netlifyLink'
+            id='netlifyLink'
+            value={project.netlifyLink}
+            type='url'
+            placeholder='Enter a Link'
+            onChange={handleInputChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for='githubLink'>GitHub Link: </Label>
+          <Input
+            name='githubLink'
+            id='githubLink'
+            value={project.githubLink}
+            type='url'
+            placeholder='Enter a Link'
+            onChange={handleInputChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for='loomLink'>Video Link: </Label>
+          <Input
+            name='loomLink'
+            id='loomLink'
+            value={project.loomLink}
+            type='url'
+            placeholder='Enter a Link'
+            onChange={handleInputChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for='description'>Description: </Label>
+          <Input
+            name='description'
+            id='description'
+            value={project.description}
+            type='text'
+            placeholder='Enter a Description'
+            onChange={handleInputChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for='techUsed'>Tech Used: </Label>
+          <Input
+            name='techUsed'
+            id='techUsed'
+            value={project.techUsed}
+            type='text'
+            placeholder='Enter a List of Tech Used'
+            onChange={handleInputChange}
+          />
+        </FormGroup>
+
+        <Button type='submit'>Submit</Button>
+      </Form>
+    </div>
+  );
+};
+
+ProjectForm.propTypes = {
+  formTitle: PropTypes.string.isRequired,
+  setProjects: PropTypes.func,
+  title: PropTypes.string,
+  screenshot: PropTypes.string,
+  netlifyLink: PropTypes.string,
+  githubLink: PropTypes.string,
+  loomLink: PropTypes.string,
+  description: PropTypes.string,
+  techUsed: PropTypes.string,
+  firebaseKey: PropTypes.string,
+};
+
+export default ProjectForm;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Jumbotron, Container } from 'reactstrap';
+
+import lightmemoji from '../assets/lightmemoji.jpg';
 
 export default function Home() {
   return (
-    <div className='user-welcome'>
-      <h1>Welcome</h1>
+    <div>
+      <Jumbotron fluid>
+        <Container fluid>
+          <div className="welcome">
+            <img className="memoji" src={lightmemoji}></img>
+          </div>
+          <div className="welcome">
+            <h1 className="display-5">
+              Welcome to Appy Trails! Your hub for hiking preparedness and
+              planning.
+            </h1>
+            <p className="lead">
+              As a guest you can browse through our planned trips or resources
+              page for ideas. Or sign in for the option to plan your own trips
+              using the resources page and planning form.
+            </p>
+          </div>
+        </Container>
+      </Jumbotron>
     </div>
   );
 }
