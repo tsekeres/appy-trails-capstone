7b11efe0e99350eb1b12c4ff878904d522777a32
tsekeres <twsekeres@gmail.com>
2021-06-17 16:52:23 -0500

    started a search bar

--- src/views/Trips.js
+++ src/views/Trips.js
@@ -2,9 +2,24 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import TripCards from '../components/TripCards';
 import { getTrips } from '../helpers/data/TripsData';
+import SearchBar from '../components/Search';
 
+const filterTripSearch = (trips, query) => {
+  if (!query) {
+    return trips;
+  }
+
+  return trips.filter((tripSearch) => {
+    const parkName = tripSearch.parkName.toLowerCase();
+    return parkName.includes(query);
+  });
+};
 function Trips({ user, admin }) {
   const [trips, setTrips] = useState([]);
+  const { search } = window.location;
+  const query = new URLSearchParams(search).get('s');
+  const [searchQuery, setSearchQuery] = useState(query || '');
+  const filteredTripSearch = filterTripSearch(trips, searchQuery);
 
   useEffect(() => {
     getTrips().then(setTrips);
@@ -15,6 +30,15 @@ function Trips({ user, admin }) {
       <div className="trips-header">
         <h1>Trip Plans</h1>
       </div>
+      <SearchBar
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+      />
+      <ul>
+        {filteredTripSearch?.map((tripSearch) => (
+          <li key={tripSearch.firebaseKey}>{tripSearch.parkName}</li>
+        ))}
+      </ul>
       <div className="card-container trips-view">
         {trips?.map((tripInfo) => (
           <TripCards
